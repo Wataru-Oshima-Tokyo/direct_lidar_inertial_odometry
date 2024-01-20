40bd88d7c14df88a4fe4d32ad2485e76d88ad458
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2024-01-20 20:13:04 +0900

    added a new fucntion to get the point cloud itself vis service

--- include/dlio/map.h
+++ include/dlio/map.h
@@ -21,6 +21,7 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include "techshare_ros_pkg2/srv/get_pcd.hpp"
 
 class dlio::MapNode: public rclcpp::Node {
 
@@ -40,7 +41,8 @@ private:
 
   void savePCD(std::shared_ptr<dlio::srv::SavePCD::Request> req,
                std::shared_ptr<dlio::srv::SavePCD::Response> res);
-
+  void getPCD(std::shared_ptr<techshare_ros_pkg2::srv::GetPCD::Request> req,
+               std::shared_ptr<techshare_ros_pkg2::srv::GetPCD::Response> res);
   rclcpp::TimerBase::SharedPtr publish_timer;
 
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr keyframe_sub;
@@ -48,6 +50,7 @@ private:
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr world_pub;
 
   rclcpp::Service<dlio::srv::SavePCD>::SharedPtr save_pcd_srv;
+  rclcpp::Service<techshare_ros_pkg2::srv::GetPCD>::SharedPtr get_pcd_srv;
 
   pcl::PointCloud<PointType>::Ptr dlio_map;
   pcl::VoxelGrid<PointType> voxelgrid;
